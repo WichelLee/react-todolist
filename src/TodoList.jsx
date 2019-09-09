a8c369f494dee48915c042b52462a270afd6e72a
WichelLee <18310038806@163.com>
2019-09-09 18:01:18 +0800

    20190909-UI组件、容器组件、无状态组件

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
+
 import store from './store/index'
 import { getInputChangeAction, saveInputValue, deleteTodoItem } from './store/actionCreators'
-
+import TodoListUI from './components/TodoListUI'
 class TodoList extends Component {
 	constructor(props) {
 		super(props);
@@ -11,36 +11,18 @@ class TodoList extends Component {
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleStoreChange = this.handleStoreChange.bind(this);
 		this.handleSaveIputValue = this.handleSaveIputValue.bind(this);
+		this.handleDeleteItem = this.handleDeleteItem.bind(this);
 		store.subscribe(this.handleStoreChange);
 	}
 	render() {
 		return (
-			<Fragment>
-				<div style={{ margin: '50px' }}>
-					<Input
-						value={this.state.inputValue}
-						placeholder="todo info"
-						style={{ width: '300px' }}
-						onChange={this.handleInputChange}
-					/>
-					<Button
-						type="primary"
-						style={{ marginLeft: '10px' }}
-						onClick={this.handleSaveIputValue}
-					>提交</Button>
-					<List
-						style={{ marginTop: '10px', width: '300px' }}
-						bordered
-						dataSource={this.state.list}
-						renderItem={(item, index) => (
-							<List.Item onClick={this.handleDeleteItem.bind(this, index)}>
-								{item}
-							</List.Item>
-						)}
-					/>
-				</div>
-
-			</Fragment>
+			<TodoListUI
+				inputValue={this.state.inputValue}
+				handleInputChange={this.handleInputChange}
+				handleSaveIputValue={this.handleSaveIputValue}
+				list={this.state.list}
+				handleDeleteItem={this.handleDeleteItem}
+			/>
 		)
 	}
 
---
+++ src/components/TodoListUI.jsx
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react'
+import { Input, Button, List } from 'antd'
+
+const TodoListUI = (props) => {
+    return (
+        <Fragment>
+            <div style={{ margin: '50px' }}>
+                <Input
+                    value={props.inputValue}
+                    placeholder="todo info"
+                    style={{ width: '300px' }}
+                    onChange={props.handleInputChange}
+                />
+                <Button
+                    type="primary"
+                    style={{ marginLeft: '10px' }}
+                    onClick={props.handleSaveIputValue}
+                >提交</Button>
+                <List
+                    style={{ marginTop: '10px', width: '300px' }}
+                    bordered
+                    dataSource={props.list}
+                    renderItem={(item, index) => (
+                        <List.Item onClick={() => { props.handleDeleteItem(index) }}>
+                            {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
+        </Fragment>
+    )
+}
+
+export default TodoListUI
