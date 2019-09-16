38022a20dd45e44ae90131abe456885f4215d78e
WichelLee <18310038806@163.com>
2019-09-16 11:33:43 +0800

    20190916-add-redux-thund and use it

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,59 +1,52 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+
 import 'antd/dist/antd.css'
 
 import store from './store/index'
-import { getInputChangeAction, saveInputValue, deleteTodoItem, initListAction } from './store/actionCreators'
+import { getInputChangeAction, saveInputValue, deleteTodoItem, getTodoList } from './store/actionCreators'
 import TodoListUI from './components/TodoListUI'
 class TodoList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = store.getState();
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleStoreChange = this.handleStoreChange.bind(this);
-		this.handleSaveIputValue = this.handleSaveIputValue.bind(this);
-		this.handleDeleteItem = this.handleDeleteItem.bind(this);
-		store.subscribe(this.handleStoreChange);
-	}
-	render() {
-		return (
-			<TodoListUI
-				inputValue={this.state.inputValue}
-				handleInputChange={this.handleInputChange}
-				handleSaveIputValue={this.handleSaveIputValue}
-				list={this.state.list}
-				handleDeleteItem={this.handleDeleteItem}
-			/>
-		)
-	}
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleSaveIputValue = this.handleSaveIputValue.bind(this);
+    this.handleDeleteItem = this.handleDeleteItem.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
+  render() {
+    return (
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        handleInputChange={this.handleInputChange}
+        handleSaveIputValue={this.handleSaveIputValue}
+        list={this.state.list}
+        handleDeleteItem={this.handleDeleteItem}
+      />
+    )
+  }
 
-	componentDidMount() {
-		axios.get('/api/todolist').then(res => {
-			if (res.status === 200 && res.statusText === 'OK') {
-				const { list } = res.data;
-				const action = initListAction(list);
-				store.dispatch(action);
-			}
-		}).catch(err => {
-			console.log(err);
-		})
-	}
+  componentDidMount() {
+    const action = getTodoList();
+    store.dispatch(action);
+  }
 
-	handleInputChange(e) {
-		const action = getInputChangeAction(e.target.value);
-		store.dispatch(action);
-	}
-	handleStoreChange() {
-		this.setState(store.getState());
-	}
-	handleSaveIputValue() {
-		const action = saveInputValue();
-		store.dispatch(action);
-	}
-	handleDeleteItem(index) {
-		const action = deleteTodoItem(index);
-		store.dispatch(action);
-	}
+  handleInputChange(e) {
+    const action = getInputChangeAction(e.target.value);
+    store.dispatch(action);
+  }
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+  handleSaveIputValue() {
+    const action = saveInputValue();
+    store.dispatch(action);
+  }
+  handleDeleteItem(index) {
+    const action = deleteTodoItem(index);
+    store.dispatch(action);
+  }
 }
 
 export default TodoList
--- src/store/actionCreators.jsx
+++ src/store/actionCreators.jsx
@@ -1,20 +1,35 @@
 import { CHANGE_INPUT_VALUE, SAVE_INPUT_VALUE, DELETE_TODO_ITEM, INIT_LIST_ACTION } from './actionTypes'
+import axios from 'axios'
 
 export const getInputChangeAction = (value) => ({
-    type: CHANGE_INPUT_VALUE,
-    value
+	type: CHANGE_INPUT_VALUE,
+	value
 })
 
 export const saveInputValue = () => ({
-    type: SAVE_INPUT_VALUE,
+	type: SAVE_INPUT_VALUE,
 })
 
 export const deleteTodoItem = (index) => ({
-    type: DELETE_TODO_ITEM,
-    index
+	type: DELETE_TODO_ITEM,
+	index
 })
 
 export const initListAction = (data) => ({
-    type: INIT_LIST_ACTION,
-    data
+	type: INIT_LIST_ACTION,
+	data
 })
+
+export const getTodoList = () => {
+	return (dispatch) => {
+		axios.get('/api/todolist').then(res => {
+			if (res.status === 200 && res.statusText === 'OK') {
+				const { list } = res.data;
+				const action = initListAction(list);
+				dispatch(action);
+			}
+		}).catch(err => {
+			console.log(err);
+		})
+	}
+}
